7ebabfad6c826fe6f9a0965f4d2cb9ca04066d5a
Rizo11 <maribjonov2002@gmail.com>
2022-09-18 07:53:52 +0300

    strongNumbers

---
+++ lab3/Lab3.c
@@ -0,0 +1,52 @@
+//
+// Created by MSI on 9/18/2022.
+//
+
+#include "Lab3.h"
+
+
+/* returns factorial of n */
+int factorialOf(int n) {
+    int result = 1;
+    if(n >= 0) {
+        for (int i = n; i > 0; i--) {
+            result *= i;
+        }
+    } else return -1;
+}
+
+
+/* checks if n is strong number or not */
+bool isStrongNumber(int n) {
+    int sumOfDigits = 0, original = n;
+
+    while (n) {
+        sumOfDigits += factorialOf(n%10);
+        n = n/10;
+    }
+
+    return sumOfDigits == original ? true : false;
+}
+
+
+/*print strong numbers from (int) n to (int) m*/
+void strongNumber() {
+    int n, m;
+    printf("Starting range  of number: ");
+    scanf("%d", &n);
+    printf("Ending range  of number: ");
+    scanf("%d", &m);
+
+    n = min(n, m);
+    m = max(n, m);
+    if(m <= 0 && n <= 0) {
+        printf("0 strong numbers\n");
+        return;
+    }
+    for (int i = n; i <= m; ++i) {
+        if(isStrongNumber(i)) {
+            printf("%d is strong number\n", i);
+        }
+    }
+
+}
---
+++ lab3/Lab3.h
@@ -0,0 +1,19 @@
+//
+// Created by MSI on 9/18/2022.
+//
+#ifndef LAB3_LAB3_H
+#define LAB3_LAB3_H
+#include "stdio.h"
+#include "stdbool.h"
+#define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
+void strongNumber();
+
+#endif //LAB3_LAB3_H
