3bf1da18b2179e596e1acfad90e300f100aea24b
Rizo11 <maribjonov2002@gmail.com>
2022-10-01 14:42:20 +0300

    random str, int

---
+++ random/library.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include "library.h"
+
+void rand_str(char *dest, size_t length) {
+    char charset[] = "0123456789"
+                     "abcdefghijklmnopqrstuvwxyz"
+                     "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    while (length-- > 0) {
+        size_t index = (double) rand() / RAND_MAX * (sizeof charset - 1);
+        *dest++ = charset[index];
+    }
+    *dest = '\0';
+}
+
+int random_number(int min_num, int max_num)
+{
+    int result = 0, low_num = 0, hi_num = 0;
+
+    if (min_num < max_num)
+    {
+        low_num = min_num;
+        hi_num = max_num + 1; // include max_num in output
+    } else {
+        low_num = max_num + 1; // include max_num in output
+        hi_num = min_num;
+    }
+    result = (rand() % (hi_num - low_num)) + low_num;
+    return result;
+}
---
+++ random/library.h
@@ -0,0 +1,9 @@
+#ifndef RANDOM_LIBRARY_H
+#define RANDOM_LIBRARY_H
+
+/* generates random string to given char* */
+void rand_str(char *dest, size_t length);
+
+/* generates random number */
+int random_number(int min_num, int max_num);
+#endif //RANDOM_LIBRARY_H
